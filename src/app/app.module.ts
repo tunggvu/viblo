f0b2af8543e6613c3320fa38822be5861c012187
tunggvu <vutungtn95@gmail.com>
2017-11-26 20:39:18 +0700

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { PeopleService } from "./people.service";
 
 import { AppComponent } from './app.component';
+import { PeopleListComponent } from './people-list/people-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PeopleListComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [PeopleService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
