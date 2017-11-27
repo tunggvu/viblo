01f27604f90e6a3b33711538cd0487db7f344004
tunggvu <vutungtn95@gmail.com>
2017-11-27 21:57:31 +0700

    people detail

--- src/app/people-list/people-list.component.ts
+++ src/app/people-list/people-list.component.ts
@@ -7,14 +7,24 @@ import { PeopleService } from "../people.service";
   template: `
     <ul>
       <li *ngFor="let person of people">
+      <a href="#" (click)="selectPerson(person)">
         {{person.name}}
+      </a>
       </li>
     </ul>
+    <section *ngIf="selectedPerson">
+    <h2>You selected:  {{selectedPerson.name}}</h2>
+    <h3>Description</h3>
+    <p>
+       {{selectedPerson.name}} weights  {{selectedPerson.weight}} and is  {{selectedPerson.height}} tall.
+    </p>
+  </section>
   `,
   styleUrls: ['./people-list.component.scss']
 })
 export class PeopleListComponent implements OnInit {
   people: Person[];
+  selectedPerson: Person;
 
   constructor(private peopleService: PeopleService) {
     this.people = peopleService.getAll();
@@ -22,4 +32,7 @@ export class PeopleListComponent implements OnInit {
 
   ngOnInit() {}
 
+  selectPerson(person){
+    this.selectedPerson = person;
+  }
 }
